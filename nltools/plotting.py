0edfe61679b97b64380a9abd2f1f13fa14d50009
caitlynlee <caitlyn.19@dartmouth.edu>
2018-05-24 11:20:25 -0500

    test

--- nltools/plotting.py
+++ nltools/plotting.py
@@ -473,7 +473,7 @@ def plot_silhouette(distance, labels, ax=None, permutation_test=True,
 
     for labelInd in range(n_clusters):
         label = labelSet[labelInd]
-        ith_cluster_silhouette_values = sample_silhouette_values[sample_silhouette_values.Label == label]['Sil'].copy()
+        ith_cluster_silhouette_values = sample_silhouette_values[labels == label]['Sil'].copy()
         ith_cluster_silhouette_values.sort_values(inplace=True)
         size_cluster_i = ith_cluster_silhouette_values.shape[0]
         x_upper = x_lower + size_cluster_i
