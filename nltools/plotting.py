3c366ab99ad3881b2954bf209507ed72e02bcfcf
caitlynlee <caitlyn.19@dartmouth.edu>
2018-05-31 12:35:33 -0400

    indexing error

--- nltools/plotting.py
+++ nltools/plotting.py
@@ -498,7 +498,8 @@ def plot_silhouette(distance, labels, ax=None, permutation_test=True,
         for labelInd in range(n_clusters):
             temp = pd.DataFrame(columns=outAll.columns)
             label = labelSet[labelInd]
-            data = sample_silhouette_values[labels == label]
+            data = sample_silhouette_values[sample_silhouette_values.Label == label]
+            
             temp.loc[labelInd,'label'] = label
             temp.loc[labelInd,'mean'] = np.mean(data)
             if np.mean(data)>0: #Only test positive mean silhouette scores
