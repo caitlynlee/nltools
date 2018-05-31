c66647cfa66ce07b1036f2924754e1bee980d19c
caitlynlee <caitlyn.19@dartmouth.edu>
2018-05-31 12:36:12 -0400

    spacing

--- nltools/plotting.py
+++ nltools/plotting.py
@@ -499,6 +499,10 @@ def plot_silhouette(distance, labels, ax=None, permutation_test=True,
             temp = pd.DataFrame(columns=outAll.columns)
             label = labelSet[labelInd]
             data = sample_silhouette_values[sample_silhouette_values.Label == label]
+
+
+
+
             
             temp.loc[labelInd,'label'] = label
             temp.loc[labelInd,'mean'] = np.mean(data)
